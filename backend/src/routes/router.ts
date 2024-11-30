f9327fbdf49ffe3373ac58268014862b219951cb
juaconsio <jconchan@outlook.com>
2024-11-30 13:58:27 -0300

    User index

--- backend/src/routes/router.ts
+++ backend/src/routes/router.ts
@@ -1,12 +1,12 @@
 import { Router } from "express";
-import usersRouter from "./users";
+import usersRouter from "./user";
 import sessionRouter from "./session";
-
+import propertiesRouter from "./property";
 const router = Router();
 router.get("/", (req, res) => {
   res.json({ message: "Hello World" });
 });
 router.use("/users", usersRouter);
 router.use("/session", sessionRouter);
-
+router.use("/properties", propertiesRouter);
 export default router;
