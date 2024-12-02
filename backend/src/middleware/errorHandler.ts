7f0ff14ce908bc6c699b74200f8f1b9694b15bae
juaconsio <jconchan@outlook.com>
2024-12-02 02:28:21 -0300

    handle error for dev

--- backend/src/middleware/errorHandler.ts
+++ backend/src/middleware/errorHandler.ts
@@ -2,17 +2,19 @@ import { ErrorRequestHandler } from 'express';
 import { ValidationError } from 'sequelize';
 
 const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
+  console.error(err);
+  console.error("Handle ERROR")
   if (err instanceof ValidationError) {
     const errors = err.errors.map((error) => ({
       field: error.path,
       message: error.message,
     }));
     res.status(400).json({ errors });
-    return;
+  } else {
+    console.error(err);
+    res.status(500).json({ message: 'Internal Server Error' });
   }
-
-  console.error(err);
-  res.status(500).json({ message: 'Internal Server Error' });
+  next();
 };
 
 export default errorHandler;
